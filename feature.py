23b33839fc61e93d35d448b5b10881b2d795e224
dingevin <Kevin1314IUNI@126.com>
2019-10-25 16:59:31 +0800

    Create feature.py

---
+++ feature.py
@@ -0,0 +1,6 @@
+1.提取mfcc特征
+  https://blog.csdn.net/zouxy09/article/details/9156785
+  https://my.oschina.net/zzw922cn/blog/544398
+  https://github.com/zzw922cn/Automatic_Speech_Recognition/blob/master/speechvalley/feature/core/sigprocess.py
+  https://blog.csdn.net/TH_NUM/article/details/80597495
+  
