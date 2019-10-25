6c6b1862bac8adf57cd53adb6342d551cc15b142
dingevin <Kevin1314IUNI@126.com>
2019-10-25 17:08:44 +0800

    Update feature.py

--- feature.py
+++ feature.py
@@ -1,4 +1,6 @@
 1.提取mfcc特征
+  https://liuyanfeier.github.io/2017/10/07/fbank%E5%92%8Cmfcc%E7%89%B9%E5%BE%81%E6%8F%90%E5%8F%96/
+  https://blog.csdn.net/Magical_Bubble/article/details/90295814
   https://blog.csdn.net/zouxy09/article/details/9156785
   https://my.oschina.net/zzw922cn/blog/544398
   https://github.com/zzw922cn/Automatic_Speech_Recognition/blob/master/speechvalley/feature/core/sigprocess.py
