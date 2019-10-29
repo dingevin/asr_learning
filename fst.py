0ebd480be6d132a2fe0c1ef32af340f9acedc88c
dingevin <Kevin1314IUNI@126.com>
2019-10-29 19:30:46 +0800

    Update fst.py

--- fst.py
+++ fst.py
@@ -1,2 +1,5 @@
 1.  fst: 有限状态传感器
-    L.fst： 
+    L.fst： 音素词典   由词典生成   phone:word
+    https://www.jianshu.com/p/4ad2add56b25
+    G.fst: 由语言模型生成      音素的3-ngram语言模型。。
+    https://blog.csdn.net/u013677156/article/details/77893661
